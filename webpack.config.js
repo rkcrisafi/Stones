9c0a145bc47da79f58a5d80a562d4dee2f1df1af
Regina Kristina Crisafi <rkcrisafi@gmail.com>
2018-01-24 13:00:42 -0500

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require("webpack");
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+module.exports = {
+  context: __dirname,
+  entry: './frontend/stones.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+};
+// webpack - transpiles code from jsx to js,.....
