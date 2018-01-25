1fe822deb12de230d10f6ee46f53becb63373bb0
Ben Blue <dalcowboiz@gmail.com>
2018-01-25 15:04:57 -0500

    Write tests for users controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,19 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
   describe 'POST #create' do
-    it 'renders the show json view' do
-      post :create
-    end
     context 'with invalid params' do
       it 'validates the presence of email and password' do
-        post :create, params: { user: { email: 'this is an invalid user' } }
-        expect(flash[:errors]).to be_present
+        post :create, format: :json, params: { user: { email: 'this is an invalid user' } }
+        expect(response).to have_http_status(422)
       end
     end
     context 'with valid params' do
       it 'validates the presence of email and password' do
-        post :create, params: { user: { email: 'this is a valid user', password: 'password' } }
+        post :create, format: :json, params: { user: { email: 'this is a valid user', password: 'password' } }
+        expect(response).to render_template('show')
+        expect(response).to have_http_status(200)
       end
     end
   end
 end
+
+
+
+# context 'new listing failure' do
+#   before do
+#     post :create, format: :json, :listing => {address: "123 Testing Lane", listing_type: "shared room", title: "Testing Listing", description: "idk", price: 1000.00, neighborhood_id: nil, host_id: 1}
+#   end
+#
+#   it 'responds with 422' do
+#     expect(response.status).to eq(422)
+#   end
+# end
