7f9a94a77c94985a2fa468990498a054a1950cbe
Ben Blue <dalcowboiz@gmail.com>
2018-02-22 00:23:34 -0500

    finish paperclip setup

--- app/views/api/rocks/index.json.jbuilder
+++ app/views/api/rocks/index.json.jbuilder
@@ -1,6 +1,7 @@
 @rocks.each do |rock|
   json.set! rock.id do
     json.extract! rock, :id, :name
+    debugger
     json.img asset_path(rock.image.url)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,20 +10,19 @@ module Stones
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_protocol => :https,
+      :s3_credentials => {
+        :bucket => ENV["s3_bucket"],
+        :access_key_id => ENV["s3_access_key_id"],
+        :secret_access_key => ENV["s3_secret_access_key"],
+        :s3_region => ENV["s3_region"]
+      }
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.generators do |g|
-      g.test_framework :rspec,
-        :fixtures => false,
-        :view_specs => false,
-        :helper_specs => false,
-        :routing_specs => false,
-        :controller_specs => true,
-        :request_specs => false
-    end
-
   end
 end
