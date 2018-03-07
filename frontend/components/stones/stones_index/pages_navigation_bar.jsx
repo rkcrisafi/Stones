01d7f3afd4463e017093617262f5d37ce92e3991
Ben Blue <dalcowboiz@gmail.com>
2018-03-06 20:21:37 -0500

    add current page and page range function

--- frontend/components/stones/stones_index/pages_navigation_bar.jsx
+++ frontend/components/stones/stones_index/pages_navigation_bar.jsx
@@ -2,17 +2,34 @@ import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import React from 'react';
 
+const pageRange = (cur, total) => {
+  if (total <= 5 || cur <= 2) {
+    return [1, 2, 3, 4, 5].slice(0, total);
+  } else if (cur >= total - 1) {
+    return [-4, -3, -2, -1, 0].map(n => n + total);
+  } else {
+    return [-2, -1, 0, 1, 2].map(n => n + cur);
+  }
+};
+
 class PagesNavBar extends React.Component {
   // we want to add page numbers up to 5;
   // the current page number should be pulled from ownProps and shown in the middle of 5 pages
   // the current page out of total number (2 of 11)
 
   render () {
+    const { rockCount, pageNum } = this.props;
     let pageCount = Math.ceil(this.props.rockCount / 20 );
+
     return (
       <div>
         <div>{this.props.rockCount} Items</div>
-        <div></div>
+        <div>{ pageNum + " of " + pageCount }</div>
+          <div>
+            <div><i class="fas fa-angle-left"></i></div>
+
+            <div><i class="fas fa-angle-right"></i></div>
+          </div>
       </div>
     );
   }
@@ -20,7 +37,10 @@ class PagesNavBar extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
+  const { search } = ownProps.location;
+  const pageNum = (search === "" ? "1" : search.match(/page=(\d+)/));
   return {
+    pageNum: Number(pageNum),
     rockCount: ownProps.rockCount,
   };
 };
