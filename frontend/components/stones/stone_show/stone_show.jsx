a371738e07f0f655c2418d6d0448666308d01e76
Ben Blue <dalcowboiz@gmail.com>
2018-03-08 23:00:33 -0500

    add basic carousel

--- frontend/components/stones/stone_show/stone_show.jsx
+++ frontend/components/stones/stone_show/stone_show.jsx
@@ -3,13 +3,26 @@ import React from 'react';
 class StoneShow extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.state = { displayed: props }
+    this.images = [];
+    this.state = { displayed: null };
   }
 
   componentDidMount() {
-    const { fetchRock, rockId } = this.props;
+    const { fetchRock, rockId, fetchImages } = this.props;
     fetchRock(rockId);
+    fetchImages(rockId);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.rock && !this.props.rock) {
+      this.images.unshift(newProps.rock.img);
+      this.forceUpdate();
+    }
+    if (newProps.images && !this.props.images) {
+      const imgSrcs = newProps.images.map(image => image.img);
+      this.images = this.images.concat(imgSrcs);
+      this.forceUpdate();
+    }
   }
 
   render() {
@@ -18,8 +31,15 @@ class StoneShow extends React.Component {
       <div className="stone-show">
         { !rock ? null : (
           <div className="rock-image-description">
-            <div className="rock-image">
-              <img src={rock.img} />
+            <div>
+              <div className="rock-image">
+                <img src={rock.img} />
+              </div>
+              <div className="img-carousel">
+                { this.images.map(url => {
+                  return <div><img src={url}/></div>;
+                })}
+              </div>
             </div>
             <div className="rock-info">
               <div className="rock-name">{rock.name}</div>
--- frontend/components/stones/stone_show/stone_show_container.js
+++ frontend/components/stones/stone_show/stone_show_container.js
@@ -1,19 +1,23 @@
 import { connect } from 'react-redux';
 import { fetchRock } from '../../../actions/rock_actions';
+import { fetchImages } from '../../../actions/image_actions';
 import StoneShow from './stone_show';
 
 const mapStateToProps = (state, ownProps) => {
   const rockId = ownProps.match.params.rockId;
   const rock = state.rocks.rocks[rockId];
-  //make image controller and fetch images with rockId
+  const images = rock ? rock.imageIds.map(id => state.images[id]) : null;
+
   return {
     rockId,
     rock,
+    images
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   fetchRock: (rockId) => dispatch(fetchRock(rockId)),
+  fetchImages: (rockId) => dispatch(fetchImages(rockId)),
 });
 
 export default connect(
