9ac5303917f7d9dbb9a8f7e5f0eefdc50a4cd393
Ben Blue <dalcowboiz@gmail.com>
2018-03-19 15:52:34 -0400

    zoom hover box works

--- frontend/components/stones/stone_show/stone_show.jsx
+++ frontend/components/stones/stone_show/stone_show.jsx
@@ -11,7 +11,8 @@ function getPos(el) {
 class StoneShow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { zoomToggle: false, zoomUrl: null };
+    this.imgContainer = [];
+    this.state = { zoomToggle: false, zoomUrl: null, boxWidth: null, boxHeight: null, boxTop: null, boxLeft: null, zoomTop: null, zoomLeft: null, zoomAmt: 3, targetIndex: null };
   }
 
   componentDidMount() {
@@ -20,39 +21,43 @@ class StoneShow extends React.Component {
     fetchImages(rockId);
   }
 
-  mouseEnter(url) {
-    this.setState({ zoomToggle: true, zoomUrl: url, boxWidth: null, boxHeight: null, boxTop: null, boxLeft: null, zoomTop: null, zoomLeft: null, zoomAmt: 3 });
+  mouseEnter(url, i) {
+    this.setState({ zoomToggle: true, zoomUrl: url, targetIndex: i });
   }
 
   mouseMove(e) {
-    const { zoomAmt } = this.state;
-    const { img } = this;
-    const { x, y } = getPos(this.img);
-    let mouseCoords = { x: e.clientX - x, y: e.clientY - y };
-    let boxSize = { width: img.width/zoomAmt, height: img.height/zoomAmt }
-    let boxCenter = { x: mouseCoords.x, y: mouseCoords.y };
-    if (mouseCoords.x < boxSize.width/2) {
-      boxCenter.x = boxSize.width/2;
-    } else if (mouseCoords.x > img.width - boxSize.width/2) {
-      boxCenter.x = img.width - boxSize.width/2;
+    const { zoomAmt, targetIndex, zoomToggle } = this.state;
+    if (zoomToggle) {
+      const { imgContainer } = this;
+      const img = imgContainer[targetIndex].childNodes[0];
+      const { x, y } = getPos(this.img);
+      let mouseCoords = { x: e.clientX - x, y: e.clientY - y };
+      let boxSize = { width: img.width/zoomAmt, height: img.height/zoomAmt }
+      let boxCenter = { x: mouseCoords.x, y: mouseCoords.y };
+      if (mouseCoords.x < boxSize.width/2) {
+        boxCenter.x = boxSize.width/2;
+      } else if (mouseCoords.x > img.width - boxSize.width/2) {
+        boxCenter.x = img.width - boxSize.width/2;
+      }
+      if (mouseCoords.y < boxSize.height/2) {
+        boxCenter.y = boxSize.height/2;
+      } else if (mouseCoords.y > img.height - boxSize.height/2) {
+        boxCenter.y = img.height - boxSize.height/2;
+      }
+      let boxPos = { x: boxCenter.x - boxSize.width / 2,
+        y: boxCenter.y - boxSize.height / 2 }
+        let zoomBoxCenter = { x: boxCenter.x * zoomAmt, y: boxCenter.y * zoomAmt };
+        let zoomBoxPos = {
+          x: zoomBoxCenter.x - boxSize.width * zoomAmt / 2,
+          y: zoomBoxCenter.y - boxSize.height * zoomAmt / 2 }
+          const boxWidth = boxSize.width;
+          const boxHeight = boxSize.height;
+          const boxTop = boxPos.y;
+          const boxLeft = boxPos.x;
+          // debugger
+
+          this.setState({ boxWidth, boxHeight, boxTop, boxLeft });
     }
-    if (mouseCoords.y < boxSize.height/2) {
-      boxCenter.y = boxSize.height/2;
-    } else if (mouseCoords.y > img.height - boxSize.height/2) {
-      boxCenter.y = img.height - boxSize.height/2;
-    }
-    let boxPos = { x: boxCenter.x - boxSize.width / 2,
-                   y: boxCenter.y - boxSize.height / 2 }
-    let zoomBoxCenter = { x: boxCenter.x * zoomAmt, y: boxCenter.y * zoomAmt };
-    let zoomBoxPos = {
-      x: zoomBoxCenter.x - boxSize.width * zoomAmt / 2,
-      y: zoomBoxCenter.y - boxSize.height * zoomAmt / 2 }
-    const boxWidth = boxSize.width;
-    const boxHeight = boxSize.height;
-    const boxTop = boxPos.y;
-    const boxLeft = boxPos.x;
-    debugger
-    this.setState({ boxWidth, boxHeight, boxTop, boxLeft });
   }
 
   render() {
@@ -65,7 +70,8 @@ class StoneShow extends React.Component {
       <div className="stone-show">
         { !rock ? null : (
           <div className="rock-image-description">
-            <div className="main-img-container">
+            <div className="main-img-container"
+              onMouseMove={(e) => this.mouseMove(e)}>
               <Carousel
                 showArrows={ true }
                 showIndicators={ false }
@@ -74,12 +80,12 @@ class StoneShow extends React.Component {
                 showStatus={ false }
                 showThumbs={ allImages.length !== 1}>
                 {allImages.map((url, i) => {
-                  return <div key={ i } ref={(el) => this.img = el} onMouseEnter={ () => this.mouseEnter(url) } onMouseLeave={ () => this.setState({ zoomToggle: false })}
-                    onMouseMove={(e) => this.mouseMove(e)}><img src={url}/></div>;
+                  return <div key={ i } ref={(el) => this.imgContainer[i] = el} onMouseEnter={ this.state.zoomToggle ? null : () => this.mouseEnter(url, i) }
+                    ><img src={url}/></div>;
                 })}
               </Carousel>
               { !this.state.zoomToggle ? null : (
-                <div className="little-box" style={{ left: boxLeft, top: boxTop, width: boxWidth, height: boxHeight }}></div>
+                <div onMouseLeave={ () => this.setState({ zoomToggle: false })} className="little-box" style={{ left: boxLeft, top: boxTop, width: boxWidth, height: boxHeight }}></div>
               )}
             </div>
             <div className="rock-info">
