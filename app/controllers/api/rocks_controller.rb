b9152939b49077e7233e4fa798c618d463c8d126
Regina Kristina Crisafi <rkcrisafi@gmail.com>
2018-03-29 00:02:32 -0400

    Add number of images per page

--- app/controllers/api/rocks_controller.rb
+++ app/controllers/api/rocks_controller.rb
@@ -2,7 +2,7 @@ class Api::RocksController < ApplicationController
 
   def index
     @item_type = params[:rock][:item_type]
-    @rocks = Rock.where(item_type: params[:rock][:item_type]).page(params[:page]).per_page(4).order('name ASC')
+    @rocks = Rock.where(item_type: params[:rock][:item_type]).page(params[:page]).per_page(20).order('name ASC')
     render :index
   end
 
