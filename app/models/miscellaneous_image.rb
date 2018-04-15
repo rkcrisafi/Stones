71667334cf6bc39ec855276d355d5958e96bccd4
Ben Blue <dalcowboiz@gmail.com>
2018-04-14 22:12:53 -0400

    add validation and paperclip stuff to miscellaneous image model

--- app/models/miscellaneous_image.rb
+++ app/models/miscellaneous_image.rb
@@ -1,2 +1,6 @@
 class MiscellaneousImage < ApplicationRecord
+  validates :type, presence: true
+
+  has_attached_file :image, default_url: "coming-soon.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
