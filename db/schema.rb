4914a82254e6f59453d138747346c4dd1992fa40
Ben Blue <dalcowboiz@gmail.com>
2018-04-14 22:10:22 -0400

    add paperclip image to miscellaneous_image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180415020047) do
+ActiveRecord::Schema.define(version: 20180415020911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20180415020047) do
     t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["type"], name: "index_miscellaneous_images_on_type"
   end
 
