411cad723c5b035fe28c539eb378b42e9e4a0a2c
Ben Blue <dalcowboiz@gmail.com>
2018-04-25 22:46:30 -0400

    add footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = (props) => {
   return (
     <div className="footer-container">
-      Footer
+      <div className="footer-social-media-links">
+        <a href="https://www.instagram.com/collectivestone/"><i className="fab fa-instagram"></i></a>
+        <a href="#"><i className="fab fa-facebook"></i></a>
+      </div>
     </div>
   );
 };
