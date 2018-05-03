7256f90e239fb7b25ac2543b74dc2734c5d433a4
Ben Blue <dalcowboiz@gmail.com>
2018-05-02 21:07:06 -0400

    add sidebar dropdown styles

--- frontend/components/sidebar/sidebar_dropdown.jsx
+++ frontend/components/sidebar/sidebar_dropdown.jsx
@@ -19,15 +19,15 @@ class SidebarDropDown extends React.Component {
     const { text, path, subTypes, subLinkClick, location } = this.props;
     const { isDown } = this.state;
     return (
-      <li>
-        <div onClick={() => this.setState({ isDown: !isDown })}>{ text }</div>
-        <div>
+      <li className="sidebar-dropdown-button">
+        <div className="sidebar-link" onClick={() => this.setState({ isDown: !isDown })}>{ text }</div>
           { !isDown ? null : (
-            subTypes.map(subType => (
-              <Link onClick={ e => this.handleClick(e, `${path}?sub-type=${subType}`, location) } to={ `${path}?sub-type=${subType}` }>{ subType }</Link>
-            ))
-          )}
-        </div>
+            <div className="sidebar-dropdown-links">
+              { subTypes.map(subType => (
+                <Link onClick={ e => this.handleClick(e, `${path}?sub-type=${subType}`, location) } to={ `${path}?sub-type=${subType}` }>{ subType }</Link>
+              )) }
+            </div>
+         )}
       </li>
     );
   }
