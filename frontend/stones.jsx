fc92737cef934b1b815ff5bee5a93c8154c2a95b
Ben Blue <dalcowboiz@gmail.com>
2018-05-17 16:58:20 -0400

    add frontend back

--- frontend/stones.jsx
+++ frontend/stones.jsx
@@ -4,15 +4,15 @@ import configureStore from './store';
 import Root from './components/root';
 const store = configureStore();
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   let store;
-//   if (window.currentUser) {
-//     const preloadedState = { session: { currentUser: window.currentUser } };
-//     store = configureStore(preloadedState);
-//     delete window.currentUser;
-//   } else {
-//     store = configureStore();
-//   }
-//   const root = document.getElementById('root');
-//   ReactDOM.render(<Root store = { store}/>, root);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store = { store}/>, root);
+});
