28f14caa343213cfef4ccfae6a080afe08384b8d
Ben Blue <dalcowboiz@gmail.com>
2018-05-16 23:05:14 -0400

    try to fix heroku webpack

--- frontend/components/tech_specs.jsx
+++ frontend/components/tech_specs.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const TechSpecs = () => {
+const TechSpecs = (props) => {
+  props
+  //webpack isn't updating
   return (
-    <div className="tech-spec">
+    <div className="tech-spec webpackisntupdatingthispageplzwork">
       <embed
         src="https://s3.amazonaws.com/collectivestone-pro/Techical+Specs+PDF.pdf"
         width="100%"
         height="100%"
         />
+      <div>hi</div>
     </div>
   );
 };
