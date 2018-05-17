2bbeedc0e54f9550556ea5f8b70fc31b095f3fd2
Ben Blue <dalcowboiz@gmail.com>
2018-05-17 18:41:59 -0400

    remove comments in store

--- frontend/store.js
+++ frontend/store.js
@@ -5,22 +5,10 @@ import RootReducer from './reducers/root_reducer';
 
 let middleware = [ thunk ]
 
-console.log(process.env.NODE_ENV);
-
 if (process.env.NODE_ENV !== 'production') {
   const { logger } = require('redux-logger');
   middleware = [ ...middleware, logger ];
 }
-
-// const store = createStore(reducer, preloadedState, applyMiddleware(...middleware))
-
-//
-// const middlewares = [thunk];
-//
-// if (process.env.NODE_ENV !== 'production') {
-//   middlewares.push(logger);
-// }
-
 
 const configureStore = (preloadedState = {}) => (
   createStore(
