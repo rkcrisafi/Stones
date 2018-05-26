73db0e121ddec29af30f84ade76978f0072b8e8e
Regina Kristina Crisafi <rkcrisafi@gmail.com>
2018-05-25 20:52:05 -0400

    Create about us functional component

---
+++ frontend/components/about_us.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <div>
+        Since ancient times the royalty and the rich surround themselves with gemstones, and it was believed they carry the highest powers of all things found in nature, like magic and healing properties, they were often ground in medicine and worn as protection. It is still believed they attract success, prosperity and love to their owners. Possessing powerful physical and metaphysical properties gemstone vibrate on multiple frequencies, purifying and balancing their wearer’s body, mind and their surroundings.
+      </div>
+      <div>
+        <div>
+          <img src="https://s3.amazonaws.com/collectivestone-pro/About+Us/Screen+Shot+2018-05-17+at+2.40.52+PM.png"/>
+        </div>
+        <div>
+          <div>
+            We are a New York City based private company, specializing in the sourcing, manufacturing and imports of ultra luxury gemstone material surfaces, consisting of handmade mosaics of natural precious and semiprecious gemstone crystals, ancient fossils and abalone shells which are engineered in a marble-like slabs.
+          </div>
+          <div>
+            These slabs are like nature’s paintings, which further enhanced by human’s artistic touch, with each gemstone carefully positioned by hand in a matrix, than bonded together, and polished to perfection for your high-end interior and exterior design projects.
+          </div>
+        </div>
+      </div>
+      <div>
+        Their beauty is desired and the effects rise unmatched. And after all, who doesn’t dream of a turquoise bath right in their home, a glowing amethyst back-lit fireplace, a fluorite table or an agate light pillar. Our designs are sleek and modern, yet timeless, inspired by the repetitive geometry and randomness the natural shapes these gemstones were usually formed, hundreds of millions of years ago in earth’s crust.
+      </div>
+      <div>
+        Out-of-this-world patterns swirl in unique colorful fractals. Backlit, solid, ultra light, or granite backed, these gems are made solid for your floors, lightweight on your walls, translucent in your bathrooms and friendly on your budget. Our technology incorporates multiple processes ensuring our materials are manufactured under the highest standards ensuring their safety and unsurpassed quality.
+      </div>
+
+      <div>
+        We source our gemstones from all over the world, including but not limited to Brazil, Australia, India, Indonesia, Tanzania, South Africa, Mozambique, Macedonia, China, Turkey and Bulgaria.
+      </div>
+
+    </div>
+  );
+};
+
+export default AboutUs;
